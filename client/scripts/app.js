ec17eb29e06e884fb4bee9b20775e6bd5febbef9
mliq <liquori@gmail.com>
2015-05-20 11:34:11 -0500

    ng-repeat working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,24 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('WelcomeController',["$scope", "$http", function($scope, $http){
+myApp.controller('WelcomeController', ["$scope", "$http", function ($scope, $http) {
     $scope.heading = "Message: ";
     $scope.persons = [];
 
-    $scope.updateMessage = function(){
+    $scope.updateMessage = function () {
         $scope.message = "Hello, " + $scope.name + "!";
     };
 
-    $scope.goodByeMessage = function(){
+    $scope.goodByeMessage = function () {
         $scope.message = "Goodbye, " + $scope.name;
     };
 
-    $scope.grabinfo = function (){
-        return $http.get('/info').then(function(response){
-           if(response.status !== 200){
-               throw new Error("I haz sad.");
-           }
+    $scope.grabinfo = function () {
+        return $http.get('/info').then(function (response) {
+            if (response.status !== 200) {
+                throw new Error("I haz sad.");
+            }
             console.log(response.data);
+            $scope.movies = response.data.movies;
             return response.data;
         });
     };
